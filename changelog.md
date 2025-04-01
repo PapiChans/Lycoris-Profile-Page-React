0b71ee2e40cd23a3a7d8b71e31251d4dc9ae93de
Christian Altiche <130991249+PapiChans@users.noreply.github.com>
2025-04-01 12:01:31 +0800

    v0.0.1

---
+++ changelog.md
@@ -0,0 +1,8 @@
+# Changelogs
+
+**v0.0.1**
+- April 1, 2025
+- Project Intialization
+- Change Icon
+- Install Taiwind
+- Customize File Structure
---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Hero() {
+    return (
+        <>
+            <h1>Hello Christian</h1>
+        </>
+    )
+}
+
+export default Hero;
